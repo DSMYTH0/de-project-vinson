39d34b84f7b46270e6731330972ccee7bc5f6a60
Mohsin Sarker <mohsin.312007@gmail.com>
2024-08-13 14:52:22 +0100

    create conncet_to_db for db connection

---
+++ src/utils/connection.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from pg8000.native import Connection
+
+load_dotenv(override=True)
+
+def connect_to_db():
+    return Connection(
+        user=os.getenv("PG_USER"),
+        password=os.getenv("PG_PASSWORD"),
+        database=os.getenv("PG_DATABASE"),
+        host=os.getenv("PG_HOST"),
+        port=int(os.getenv("PG_PORT"))
+    )
+    
