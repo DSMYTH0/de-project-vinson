28016cc8615538bb4dcecfff3e167d6ae8624f76
Elyman1 <elymanwork@gmail.com>
2024-08-15 15:00:31 +0100

    introduce necessary requirements

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -10,6 +10,39 @@ resource "aws_s3_object" "lambda_code" {
   source = "${path.module}/../packages/extract/function.zip"
 }
 
+
+
+resource "null_resource" "create_dependencies" {
+  provisioner "local-exec" {
+    command = "pip install -r ${path.module}/../requirements.txt -t ${path.module}/../dependencies/python"
+  }
+
+  triggers = {
+    dependencies = filemd5("${path.module}/../requirements.txt")
+  }
+}
+
+data "archive_file" "extract_lambda_dependencies_zip" {
+  type        = "zip"
+  output_path = "${path.module}/../packages/layers/dependencies.zip"
+  source_dir = "${path.module}/../dependencies"
+}
+
+resource "aws_s3_object" "lambda_requirements_layer_s3" {
+  bucket = aws_s3_bucket.code_bucket.bucket
+  key = "packages/extract/dependencies.zip"
+  source = "${path.module}/../packages/layers/dependencies.zip"
+  etag = filemd5(data.archive_file.extract_lambda_dependencies_zip.output_path)
+}
+
+
+resource "aws_lambda_layer_version" "lambda_dependencies_layer" {
+  layer_name = "lambda_dependencies_layer"
+  s3_bucket = aws_s3_object.lambda_requirements_layer_s3.bucket
+  s3_key = aws_s3_object.lambda_requirements_layer_s3.key
+}
+
+
 resource "aws_lambda_function" "extract_lambda" {
     function_name = var.extract_lambda_func
     s3_bucket = aws_s3_bucket.code_bucket.bucket
@@ -17,6 +50,7 @@ resource "aws_lambda_function" "extract_lambda" {
     role = aws_iam_role.extract_lambda_role.arn
     handler = "extract.${var.extract_lambda_func}"
     runtime = "python3.12"
+    layers = [aws_lambda_layer_version.lambda_dependencies_layer.arn]
 
     environment {
     variables = {
